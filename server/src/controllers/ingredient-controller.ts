fdc231ca088ce13dec782b80a3b34a61ffbc1df9
Oli Stogdale <oli.stogdale@gmail.com>
2025-06-06 22:12:52 +0100

    Updated recipes.json to contain additional mock recipe data for prototyping (non-seasonal)

---
+++ server/src/controllers/ingredient-controller.ts
@@ -0,0 +1,15 @@
+// import { Context } from "koa";
+
+// import ingredients from '../models/ingredient-model';
+
+// const getSeasonalIngredients = async (ctx: Context) => {
+//   const {month} = ctx.request.body;
+//   try {    
+//     ctx.body = await ingredients.find({
+
+//     });
+//     ctx.status = 200;
+//   } catch (error) {
+//     ctx.status = 500;
+//   }
+// }
--- server/src/routers/ingredient-router.ts
+++ server/src/routers/ingredient-router.ts
@@ -3,9 +3,14 @@ import Router from 'koa-router'
 const ingredientRouter = new Router()
 
 let cb = () => {
-  return ['tomato', 'mozzarella', 'flour'];
+  return ['tomato', 'aubergine', 'mozzarella', 'flour'];
+}
+
+let cb2 = () => {
+  return ['tomato', 'flour', 'mozzarella']
 }
 
 ingredientRouter.get('./ingredients', cb)
+ingredientRouter.get('./ingredients/:month', cb2)
 
 export default ingredientRouter;
