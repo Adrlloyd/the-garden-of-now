01aa6e9200e684f383c4ce49c75300dac44891fc
Oli Stogdale <oli.stogdale@gmail.com>
2025-06-09 16:59:02 +0100

    Basic functionality to fetch seasonal ingredient list on app load implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+// import NavBar from './NavBar/NavBar.jsx'
+// import Dashboard from './Dashboard/Dashboard.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+  
+  const [month, setMonth] = useState(months[new Date().getMonth()])
+  const [seasonalIngredients, setSeasonalIngredients] = useState([])
+
+  useEffect(() => {
+
+    const fetchEvents = async () => {
+      const url = `http://127.0.0.1:3000/ingredients/${month}`;
+      try {
+        const response = await fetch(url);
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`)
+        };
+        const body = await response.json();
+        console.log(body);
+        setSeasonalIngredients(body);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchEvents();
+  }, [month]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <p>{seasonalIngredients}</p>
   )
 }
 
