4c150e23ed268399f2fab287294dfadfec05b377
María Victoria Doll <mvictoriadoll@gmail.com>
2025-06-12 14:46:38 +0200

    Migrate App, NavBar and views to TypeScript

--- client/src/view/views.js
+++
@@ -1,8 +0,0 @@
-const VIEWS = {
-  HOME: 'home',
-  RECIPE_LIST: 'recipe-list',
-  RECIPE_DETAIL: 'recipe-detail',
-  FAVOURITES_LIST: 'favourites-list',
-};
-
-export { VIEWS }
---
+++ client/src/view/views.ts
@@ -0,0 +1,8 @@
+const VIEWS = {
+  HOME: 'home',
+  RECIPE_LIST: 'recipe-list',
+  RECIPE_DETAIL: 'recipe-detail',
+  FAVOURITES_LIST: 'favourites-list',
+};
+
+export { VIEWS }
