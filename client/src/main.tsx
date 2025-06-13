04ee5c9480acab4a8792017b37fd727092bfe3b0
María Victoria Doll <mvictoriadoll@gmail.com>
2025-06-13 13:14:43 +0200

    fix html label

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './App.jsx'
+import App from './App'
 
-const root = document.getElementById('root') as HTMLElement;
+const root = document.getElementById('root')! as HTMLElement;
 
 createRoot(root).render(
   <StrictMode>
     <div className="wrapper">
       <App />
     </div>
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
